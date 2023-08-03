47980275f152abbbaa7a9c27cd7bd650465bb809
liwen <liwen@gzntkj.com>
2023-08-03 10:38:37 +0800

    fix

--- file.go
+++ file.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	var port int
-	flag.IntVar(&port, "-p", 2023, "端口")
+	flag.IntVar(&port, "p", 2023, "端口")
 	flag.Parse()
 	proxy.DProxy.CreateFileServerByGin("/file", "", fmt.Sprintf("%v", port), false)
 }
