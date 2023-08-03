0accaf1448621218db7ebef230d351e0fba689aa
liwen <liwen@gzntkj.com>
2023-08-03 11:30:02 +0800

    fix

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 }
 
 func Run() {
-	log.Println("version:1.0.0")
+	log.Println("version:1.0.1")
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	var port string
 	var device string
